754bd92ba05c505617e41929f89199aec230b92c
Kayrat-Ti <80088962+Kayrat-85@users.noreply.github.com>
2021-10-25 01:23:48 +0600

    Add files via upload

---
+++ WebUI/Models/BooksListViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Domain.Entities;
+
+namespace WebUI.Models
+{
+    public class BooksListViewModel
+    {
+        public IEnumerable<Book> Books { get; set; }
+        public PagingInfo PagingInfo { get; set; }
+        //текущая выбранная категория
+        public string CurrentGenre { get; set; }
+    }
+}
