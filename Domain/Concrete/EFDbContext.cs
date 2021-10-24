2f2b2dbb1ac6acbe162e265e03509ededc0f0e11
Kayrat-Ti <80088962+Kayrat-85@users.noreply.github.com>
2021-10-25 00:19:08 +0600

    Add files via upload

---
+++ Domain/Concrete/EFDbContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using Domain.Entities;
+
+namespace Domain.Concrete
+{
+    public class EFDbContext : DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+    }
+}
